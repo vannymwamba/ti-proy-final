36cf6aee0c544978e50a904f05c4946c0ef9d4d0
emir.herrera <emir.herrera@gmail.com>
2010-12-04 01:46:25 +0000

    Clase coeficiente documentada

--- CompresorChebyshev/src/compresorchebyshev/Coeficiente.java
+++ CompresorChebyshev/src/compresorchebyshev/Coeficiente.java
@@ -17,16 +17,16 @@ public class Coeficiente {
     private boolean overflow;
     
     /**
-     * Default constructor
+     * Contructor por default
      */
     public Coeficiente(){
         value = new byte[3];
     }
 
     /**
-     * Constructor that receives the byte array representation of the float point number
+     * Inicializa al coeficiente con el valor indicado
      * @param byteArray
-     * @deprecated
+     * Arreglo de bytes codificado como punto flotante de 24 bits 1:4:19
      */
     public Coeficiente(byte[] byteArray){
         value = new byte[3];
@@ -34,22 +34,29 @@ public class Coeficiente {
     }
 
     /**
-     * Constructor that receives the hexadecimal representation of the float number
+     * Inicializa el valor del Coeficiente
      * @param value
+     * Valor hexadecimal en formto de punto flotante de 24 bits
      */
     public Coeficiente(long value){
         setValue(value);
     }
 
     /**
-     * Constructor that receives the base 10 representation of the number
+     * Inicializa el Coeficiente con valor decimal
      * @param value
+     * Valor decimal del coeficiento
      */
     public Coeficiente(double value){
 
         setValue(value);
     }
-    
+
+    /**
+     * Establece un valor para el coeficiente
+     * @param value
+     * Representacion hexadecimal del Coeficiente en foamto de punto flotante de 3 bytes
+     */
     /**
      *
      * @param value
@@ -62,8 +69,9 @@ public class Coeficiente {
     }
 
     /**
-     *
+     * Establece un valor para el coeficiente
      * @param value
+     * Representación decimal del coeficiente
      */
     public final void setValue(double value){
         this.value = new byte[3];
@@ -85,8 +93,8 @@ public class Coeficiente {
         if (exp >= -8 && exp <= 7){
 
             valExp = Math.pow(2, exp);
-            double mantiza = (value - valExp)/valExp;
-            setMantiza(mantiza);
+            double mantisa = (value - valExp)/valExp;
+            setMantisa(mantisa);
             setExponent(exp);            
         }
         else{
@@ -106,18 +114,19 @@ public class Coeficiente {
         }
     }
 
-    /*
+    /**
+     * Verifica el signo del coeficiente
      * @return
-     *  True if the number is negative, false otherwhise
-     *
+     *  True si el número es negativo, falso de otra forma
      */
     private boolean isNegative(){
         return (value[0] & 0x80) == 0x80;
     }
 
-    /*
+    /**
+     * Establece el signo del número
      * @param neg
-     *  True if the numis wil be set as negative, false otherwhise
+     *  True si el número es negativo, falso de otra forma
      */
     private void setNegative(boolean neg){
         if (neg)
@@ -126,11 +135,20 @@ public class Coeficiente {
             value[0] = (byte) (value[0] & 0x7F);
     }
 
-
+    /**
+     * Obtiene el valor del exponente
+     * @return
+     * Valor del exponente entr -8 y 7
+     */
     private int getExponent(){
         return ((value[0] & 0x78)>>> 3) - 8;
     }
 
+    /**
+     * Establece el valor del exponente
+     * @param exp
+     * Valor del exponente entre -8 y 7
+     */
     private void setExponent(int exp){
         if (exp >= -8 && exp <=7){
             exp += 8;
@@ -141,7 +159,12 @@ public class Coeficiente {
 
     }
 
-    private double getMantiza(){
+    /**
+     * Devuelve el valor de la mantisa
+     * @return
+     * Un double con el valor de la mantisa
+     */
+    private double getMantisa(){
         int posVal;
         String binaryVal, aux;
 
@@ -167,24 +190,24 @@ public class Coeficiente {
     }
 
     /**
-     * Sets the mantiza a new value
-     * @param mantiza
-     * The decimal value for the mantiza
+     * Asigna un valor a la mantisa
+     * @param mantisa
+     * Double con el nuevo valor de la mantisa
      */
 
-    public final void setMantiza(double mantiza){
-        int binMantiza;
+    public final void setMantisa(double mantisa){
+        int binMantisa;
         double smallest = Math.pow(2, -19);
-        if (mantiza >=0 && mantiza <= 1 - smallest){
+        if (mantisa >=0 && mantisa <= 1 - smallest){
             value [0] = (byte) (value[0] & 0xF8);
             value [1] = 0;
             value [2] = 0;
 
-            binMantiza = (int) Math.round(mantiza / smallest);
+            binMantisa = (int) Math.round(mantisa / smallest);
 
-            value [0] = (byte) (value[0] | ((binMantiza >>> 16) & 0x07));
-            value [1] = (byte) ((binMantiza >>> 8) & 0xFF);
-            value [2] = (byte) (binMantiza & 0xFF);
+            value [0] = (byte) (value[0] | ((binMantisa >>> 16) & 0x07));
+            value [1] = (byte) ((binMantisa >>> 8) & 0xFF);
+            value [2] = (byte) (binMantisa & 0xFF);
 
         }else
             System.err.println("La mantiza no tiene un valor válido");//Arroja excepción de mantiza inválida
@@ -192,13 +215,14 @@ public class Coeficiente {
     }
 
     /**
-     *
+     * Obtiene la representación decimal del Coeficiente
      * @return
+     * Double con el valor decimal
      */
     public double toDouble(){
         double dValue;
         
-        dValue = Math.pow(2, getExponent()) * (1 + getMantiza());
+        dValue = Math.pow(2, getExponent()) * (1 + getMantisa());
 
         if (isNegative())
             dValue = -dValue;
@@ -206,8 +230,9 @@ public class Coeficiente {
     }
 
     /**
-     *
+     * Representación del Coeficiente como String
      * @return
+     * Valor decimal del coeficiente como String
      */
     @Override
     public String toString(){
@@ -215,40 +240,61 @@ public class Coeficiente {
     }
 
     /**
-     *
+     * Obtiene la codificación binaria del Coeficiete
      * @return
+     * Un String con la representación binaria del número codificado en el coeficiente
      */
     public String toBinaryString(){
-        return Integer.toBinaryString(((value[0] & 0xFF) << 16) | (value[1] & 0xFF) << 8 | value[2] & 0xFF);
+        String binaryVal, aux;
+
+        aux = Integer.toBinaryString(value[0] & 0x07);
+        while (aux.length()<3)
+            aux = "0" + aux;
+
+        binaryVal = aux;
+        aux =  Integer.toBinaryString(value[1] & 0xFF);
+        while (aux.length()<8)
+            aux = "0" + aux;
+        binaryVal += aux;
+
+        aux =  Integer.toBinaryString(value[2] & 0xFF);
+        while (aux.length()<8)
+            aux = "0" + aux;
+        binaryVal += aux;
+        return binaryVal;
     }
 
     /**
-     *
+     * Obtiene la representación en Hexadecimal del número codificado en el coeficiente
      * @return
+     * String con la representación en Hexadecimal del número en punto flotante
      */
     public String toHexString(){
         return Integer.toHexString(((value[0] & 0xFF) << 16) | (value[1] & 0xFF) << 8 | value[2] & 0xFF);
     }
 
     /**
-     *
+     * Cuando se asigna un valor menor al rango de valores del coeficiente se produce un underflow y el número se mapea al menor que puede almacenarse en un coeficiente
      * @return
+     * True si el valor real es menor a la cota inferior
      */
     public boolean isUnderflow(){
         return underflow;
     }
 
     /**
-     *
+     * Cuando se asigna un valor mayor al rango de valores del coeficiente se produce un underflow y el número se mapea al mayor que puede almacenarse en un coeficiente
      * @return
+     * True si el valor real es mayor a la cota superior
      */
     public boolean isOverflow(){
         return overflow;
     }
 
     /**
-     *
+     * Obtiene un arreglo de bytes que contiene la codficicación del coeficiente
      * @return
+     * Un arreglo de 3 bytes con el número en punto flotante
      */
     public byte[] getAsByteArray(){
         return value;
